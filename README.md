e0579f4430d2158c1c7239f6933394f99ac7a787
cloud <62519659+respecting@users.noreply.github.com>
2021-04-19 16:52:15 -0600

    generator less go

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# rickroll
- cool tool
+# discord-games
+A way to bait your friends with a "new Discord feature!"
+
+# Live Demo
+[Demo](https://discord-games.respecting.repl.co/)
+
+# Usage
+1. Clone this repository
+2. CD into this repository
+3. `npm i`
+4. `node index.js`
+5. Profit!
+
+# Port is 1935 by default.
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const fetch = require('node-fetch');
+const FormData = require('form-data')
+const { createCanvas, loadImage } = require('canvas');
+const fileUpload = require('express-fileupload');
 const parser = bodyParser.urlencoded({
     extended: false
 });
@@ -21,9 +25,63 @@ const app = express();
 const port = 1935;
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir : '/tmp/'
+}));
 
 app.get('/', (req, res) => {
     res.render("landing")
+})
+
+app.post('/generator', (req, res) => {
+    if (!/http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/g.test(req.body.youtubeLink)) {
+        return res.status(400).send(`<h1>Input a valid Youtube link!</h1><meta http-equiv="refresh" content="3;url=generator.html"/>`)
+    }
+    if (!req.files.image) {
+        return res.status(400).send(`<h1>lol</h1><meta http-equiv="refresh" content="3;url=generator.html"/>`)
+    }
+    const canvas = createCanvas(1250, 703)
+    const ctx = canvas.getContext("2d");
+    
+    const renderStuff = async () => {
+        loadImage("public/disc_games.png").then(i=>{
+            ctx.drawImage(i, 0, 618);
+        })
+        const center = await loadImage("public/center.png").then(i=>{
+            ctx.drawImage(i, 502, 273);
+        })
+        let body = new FormData()
+        body.append("image", canvas.toDataURL().split(',')[1])
+        fetch("https://api.imgur.com/3/image", {
+            method: "POST",
+            headers: {
+                    "Authorization": "Client-ID 5fae4323a27c0cf",
+                    "Content-Length": canvas.toDataURL().split(',')[1].length
+            },
+            body: body,
+            redirect: "follow"
+        }).then(r=>r.json()).then(json=>{
+            res.render("generator", {
+                embed: `<!DOCTYPE html>
+<html>
+    <head>
+        <meta property="og:type" content="video.other">
+        <meta property="twitter:player" content="https://www.youtube.com/embed/${new URLSearchParams(new URL(req.body.youtubeLink).search).get("v")}">
+        <meta property="og:video:type" content="text/html">
+        <meta property="og:video:width" content="900">
+        <meta property="og:video:height" content="506">
+        <meta name="twitter:image" content="${json.data.link}">
+        <meta http-equiv="refresh" content="0;url=${req.body.youtubeLink}"/>
+    </head>
+</html>`})
+        })
+    }
+    loadImage(req.files.image.tempFilePath).then(i=>{
+        ctx.drawImage(i, 0, 0, 1250, 703);
+        renderStuff();
+    })
 })
 
 app.post('/create', parser, (req, res) => {
@@ -59,11 +117,11 @@ app.post('/create', parser, (req, res) => {
 })
 
 app.get('/get', (req, res) => {
-    if (!req.get("User-Agent").includes("discord")) return res.redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
     db.findOne({
         _id: req.query.id
     }, function(err, doc) {
         if (!doc) return;
+        if (!req.get("User-Agent").includes("discord")) return res.redirect(doc.youtube);
         res.render("index", {
             yt: new URLSearchParams(new URL(doc.youtube).search).get("v"),
             image: doc.image,
